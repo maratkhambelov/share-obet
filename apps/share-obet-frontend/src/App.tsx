a821d66c4e3c1a8f92a72e99845d3598acc7cf8e
MaratKhambelov <maratkhambelov@gmail.com>
2026-05-28 12:23:25 +0300

    init with test call api tma.js/sdk

--- apps/share-obet-frontend/src/App.tsx
+++ apps/share-obet-frontend/src/App.tsx
@@ -1,18 +1,56 @@
 import {useEffect} from 'react'
 import './App.css'
 // import WebApp from "@twa-dev/sdk";
+// import type { WebApp  } from "@tma.js/types";
+
+// export const tg = window.Telegram?.WebApp;
+//
+
+import { init } from "@tma.js/sdk";
+import { postEvent, mainButton } from "@tma.js/sdk";
+import { useLaunchParams } from "@tma.js/sdk-react";
+
+
+init()
 
 function App() {
   // const [count, setCount] = useState(0)
+    const { tgWebAppData } = useLaunchParams();
+
+    const user = tgWebAppData?.user
 
     useEffect(() => {
-        // WebApp.ready();
+        console.log(user);
     }, [])
 
-  return (
+    useEffect(() => {
+        postEvent("web_app_expand");
+    }, [])
+
+    useEffect(() => {
+        mainButton.setParams({
+            text: "Create pledge",
+            isVisible: true,
+        });
+
+        const off = mainButton.onClick(() => {
+            console.log("click");
+        });
+
+        return () => {
+            off();
+            mainButton.hide();
+        };
+    }, []);
+
+
+
+    return (
     <>
         PRIVET!!!!!!!!!!!!!!
-        {/*User: {WebApp.initDataUnsafe.user?.first_name}*/}
+        <p>{user?.first_name}</p>
+        <p>@{user?.username}</p>
+        <p>{user?.id}</p>
     </>
   )
 }
