32ad75ef4210971800fb3f4fce0ef0bb63470b3b
MaratKhambelov <maratkhambelov@gmail.com>
2026-05-28 12:53:19 +0300

    fix base template for tg web mini app

--- apps/share-obet-frontend/src/App.tsx
+++ apps/share-obet-frontend/src/App.tsx
@@ -1,45 +1,50 @@
 import {useEffect} from 'react'
 import './App.css'
-// import WebApp from "@twa-dev/sdk";
-// import type { WebApp  } from "@tma.js/types";
-
-// export const tg = window.Telegram?.WebApp;
-//
 // https://www.npmjs.com/package/@ton/ton?utm_source=chatgpt.com
 // https://www.npmjs.com/package/@tonconnect/ui-react?utm_source=chatgpt.com
 
-import { postEvent, mainButton } from "@tma.js/sdk";
+import {
+    mainButton,
+    miniApp } from "@tma.js/sdk";
 import { useLaunchParams } from "@tma.js/sdk-react";
 
 
 
 function App() {
-  // const [count, setCount] = useState(0)
     const { tgWebAppData } = useLaunchParams();
 
     const user = tgWebAppData?.user
 
     useEffect(() => {
         console.log(user);
-    }, [])
+    }, [user])
 
     useEffect(() => {
-        postEvent("web_app_expand");
-    }, [])
+        miniApp.mount();
+
+        miniApp.ready();
+
+        // return () => {
+        //     miniApp.unmount();
+        // };
+    }, []);
 
     useEffect(() => {
+        mainButton.mount();
+
         mainButton.setParams({
             text: "Create pledge",
             isVisible: true,
         });
 
         const off = mainButton.onClick(() => {
-            console.log("click");
+            console.log("Create pledge click");
         });
 
         return () => {
             off();
-            mainButton.hide();
+
+            // mainButton.unmount();
         };
     }, []);
 
@@ -47,10 +52,53 @@ function App() {
 
     return (
     <>
+        {!user && (
+            <p>
+                Open inside Telegram
+            </p>
+        )}
+
         PRIVET!!!!!!!!!!!!!!
-        <p>{user?.first_name}</p>
-        <p>@{user?.username}</p>
-        <p>{user?.id}</p>
+        {user && (
+            <>
+                {user.photo_url && (
+                    <img
+                        src={user.photo_url}
+                        alt={user.first_name}
+                        width={96}
+                        height={96}
+                        style={{
+                            borderRadius: "50%",
+                        }}
+                    />
+                )}
+
+                <p>
+                    {user.first_name}{" "}
+                    {user.last_name}
+                </p>
+
+                <p>
+                    @{user.username}
+                </p>
+
+                <p>
+                    ID: {user.id}
+                </p>
+
+                <p>
+                    Premium:{" "}
+                    {user.is_premium
+                        ? "YES"
+                        : "NO"}
+                </p>
+
+                <p>
+                    Language:{" "}
+                    {user.language_code}
+                </p>
+            </>
+        )}
     </>
   )
 }
